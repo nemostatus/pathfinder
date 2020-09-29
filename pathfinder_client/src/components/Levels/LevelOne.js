d29772b911e0b1e60c9a47a006f102baf63bf49f
Matthew <mattcorralejo1@gmail.com>
2020-09-29 14:21:56 -0700

    created router component and I've built out part of level one

---
+++ pathfinder_client/src/components/Levels/LevelOne.js
@@ -0,0 +1,36 @@
+
+import React from 'react'
+
+export default function LevelOne()  {
+  
+        return (
+            <div>
+
+            <div class="situation-container">
+                <div class ="situation">
+                 
+              <strong> You are this.username(mapstatetoprops giving this users username ) the bravest knight in the kingdom. 
+                As you are resting in your quarters, you're door is kicked in and as you go to reach for your sword, you 
+                recognize these intruders as royal guards. "You must come with us (this.username), the king has demanded 
+                your assistance at once." You ride out on your horse to the castle. As soon as you enter the king shouts
+                furiously, "(this.username)! Princess Redux is missing! Some of my guards believe she was taken sometime 
+               last night! You must save her. Or it will be your head!" As you ride out towards the forest you see a damsel
+               in distress being attacked by some goblins!
+               </strong>
+               </div>
+                
+               <div class = "option1">
+           option 1
+               </div>
+
+               <div class = "option2">
+                   option 2
+               </div>
+               </div>
+            </div>
+
+           
+        )
+    }
+
+
--- pathfinder_client/src/components/Router.js
+++ pathfinder_client/src/components/Router.js
@@ -3,12 +3,14 @@ import {Switch,Route} from 'react-router-dom'
 import Usersform from './Usersform'
 import About from './About'
 import GamesList from './GamesList'
+import LevelOne from './Levels/LevelOne'
 const Router = () => {
     return(
         <Switch>
             <Route exact path="/" component={Usersform}/>
             <Route exact path="/about" component={About}/>
             <Route exact path="/games" component={GamesList}/>
+            <Route exact path="/level/1" component={LevelOne}/>
             </Switch>
     )
 }

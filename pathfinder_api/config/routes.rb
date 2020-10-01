65063259b30998ceea4ddbce836ef07d02ac77d6
Matthew <mattcorralejo1@gmail.com>
2020-10-01 08:46:32 -0700

    creating sessions and registrations controllers

--- pathfinder_api/config/routes.rb
+++ pathfinder_api/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: "static#home"
   resources :users
   resources :games
-  resources :sessions
+  resources :sessions, only: [:create]
+  resources :registration, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

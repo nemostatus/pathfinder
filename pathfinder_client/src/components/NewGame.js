bc2e09358424e4a2edcd6130e5d16a749a25dc49
Matthew <mattcorralejo1@gmail.com>
2020-10-06 11:10:19 -0700

    switching to a shorter story

--- pathfinder_client/src/components/NewGame.js
+++ pathfinder_client/src/components/NewGame.js
@@ -13,15 +13,10 @@ import ls from 'local-storage'
         hearts: 5,
 
         user_id: ls.get('user_id'),
-        choice1: 'Save the damsel!',
-        choice2: ' Move forward into the forest.',
-        prompt: 'You are this.username(mapstatetoprops giving this users username ) the bravest knight in the kingdom. \
-        As you are resting in your quarters, youre door is kicked in and as you go to reach for your sword you  \
-        recognize these intruders as royal guards. "You must come with us (this.username), the king has demanded \
-        your assistance at once." You ride out on your horse to the castle. As soon as you enter the king shouts \
-        furiously, "(this.username)! Princess Redux is missing! Some of my guards believe she was taken sometime \
-       last night! You must save her. Or it will be your head!" As you ride out towards the forest you see a damsel \
-       in distress being attacked by some goblins!'
+        choice1: 'Order the pilot to manuever through the belt',
+        choice2: 'Oreder the pilot to use the hyper-beam',
+        prompt: 'The year is 3030.You are the cpt. of a space mission. Your mission is to head from the mars base to Pluto\
+        With everything going to plan you hear the pilot yell out that the ships is heading toward an asteroid belt.You..'
 
         }
 
